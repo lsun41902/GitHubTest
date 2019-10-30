c27c5129aa0d748b1703d43e1fe0375ba77728ba
lsun41902 <57166289+lsun41902@users.noreply.github.com>
2019-10-30 15:50:47 +0900

    Update MainActivity.java

--- app/src/main/java/com/lsun/githubtest/MainActivity.java
+++ app/src/main/java/com/lsun/githubtest/MainActivity.java
@@ -13,5 +13,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         tv=findViewById(R.id.tv);
+        tv.settext("Nice to meet you");
     }
 }
